9f8388391362e545b8d1d7e06951f24b5b8e2255
Gomes Contra Incêndio <comercialgomescontraincendio@gmail.com>
2025-06-16 19:04:06 +0000

    modifique as opções dentro do botão flutuante de ações para que não tenh

--- src/components/app/action-buttons-panel.tsx
+++ src/components/app/action-buttons-panel.tsx
@@ -14,7 +14,7 @@ import {
   DropdownMenuPortal,
 } from "@/components/ui/dropdown-menu";
 import { 
-  Settings2, // Main FAB icon
+  Settings2, 
   Save, 
   PlusSquare, 
   Printer, 
@@ -30,6 +30,8 @@ import {
   Eye, 
   EyeOff 
 } from 'lucide-react';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { cn } from '@/lib/utils';
 
 interface ActionButtonsPanelProps {
   onSave: () => void;
@@ -60,6 +62,8 @@ export function ActionButtonsPanel({
   onGeneratePdf,
   onGeneratePhotoReportPdf,
 }: ActionButtonsPanelProps) {
+  const isMobile = useIsMobile();
+
   return (
     <div className="fixed bottom-6 right-6 z-50">
       <DropdownMenu>
@@ -75,79 +79,82 @@ export function ActionButtonsPanel({
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuPortal>
-          <DropdownMenuContent align="end" sideOffset={10} className="w-64">
-            <DropdownMenuLabel>Ações da Vistoria</DropdownMenuLabel>
-            <DropdownMenuSeparator />
+          <DropdownMenuContent align="end" sideOffset={10} className={cn("w-auto min-w-[56px]", !isMobile && "w-64")}>
+            <DropdownMenuLabel className={cn(isMobile && "hidden")}>Ações da Vistoria</DropdownMenuLabel>
+            {!isMobile && <DropdownMenuSeparator />}
 
             <DropdownMenuItem onClick={onSave} className="cursor-pointer">
-              <Database className="mr-2 h-4 w-4" />
-              <span>Salvar Vistoria</span>
+              <Database className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Salvar Vistoria</span>}
             </DropdownMenuItem>
 
             <DropdownMenuItem onClick={onToggleSavedInspections} className="cursor-pointer">
-              {isSavedInspectionsVisible ? <EyeOff className="mr-2 h-4 w-4" /> : <Eye className="mr-2 h-4 w-4" />}
-              <span>{isSavedInspectionsVisible ? "Ocultar Salvas" : "Ver Salvas"}</span>
+              {isSavedInspectionsVisible ? 
+                <EyeOff className={cn("h-4 w-4", !isMobile && "mr-2")} /> : 
+                <Eye className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              }
+              {!isMobile && <span>{isSavedInspectionsVisible ? "Ocultar Salvas" : "Ver Salvas"}</span>}
             </DropdownMenuItem>
 
             <DropdownMenuItem onClick={onAddNewTower} className="cursor-pointer">
-              <Building className="mr-2 h-4 w-4" />
-              <span>Nova Torre</span>
+              <Building className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Nova Torre</span>}
             </DropdownMenuItem>
             
             <DropdownMenuSeparator />
 
             <DropdownMenuSub>
               <DropdownMenuSubTrigger>
-                <FileDown className="mr-2 h-4 w-4" />
-                <span>Baixar Relatório PDF</span>
+                <FileDown className={cn("h-4 w-4", !isMobile && "mr-2")} />
+                {!isMobile && <span>Baixar Relatório PDF</span>}
               </DropdownMenuSubTrigger>
               <DropdownMenuPortal>
-                <DropdownMenuSubContent className="w-56">
-                  <DropdownMenuLabel>Tipos de Relatório PDF</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
+                <DropdownMenuSubContent className={cn("w-auto min-w-[56px]", !isMobile && "w-56")}>
+                  <DropdownMenuLabel className={cn(isMobile && "hidden")}>Tipos de Relatório PDF</DropdownMenuLabel>
+                  {!isMobile && <DropdownMenuSeparator />}
                   <DropdownMenuItem onClick={onGenerateRegisteredItemsReport} className="cursor-pointer">
-                    <FileSpreadsheet className="mr-2 h-4 w-4" />
-                    <span>Itens Cadastrados</span>
+                    <FileSpreadsheet className={cn("h-4 w-4", !isMobile && "mr-2")} />
+                    {!isMobile && <span>Itens Cadastrados</span>}
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={onGenerateNCItemsReport} className="cursor-pointer text-orange-600 focus:text-orange-700">
-                    <AlertTriangle className="mr-2 h-4 w-4" />
-                    <span>Itens N/C</span>
+                    <AlertTriangle className={cn("h-4 w-4", !isMobile && "mr-2")} />
+                    {!isMobile && <span>Itens N/C</span>}
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={onGeneratePhotoReportPdf} className="cursor-pointer">
-                    <ImageIcon className="mr-2 h-4 w-4" />
-                    <span>Somente Fotos</span>
+                    <ImageIcon className={cn("h-4 w-4", !isMobile && "mr-2")} />
+                    {!isMobile && <span>Somente Fotos</span>}
                   </DropdownMenuItem>
-                  <DropdownMenuSeparator />
+                  {!isMobile && <DropdownMenuSeparator />}
                   <DropdownMenuItem onClick={onGeneratePdf} className="cursor-pointer">
-                    <FileText className="mr-2 h-4 w-4" />
-                    <span>Relatório Completo</span>
+                    <FileText className={cn("h-4 w-4", !isMobile && "mr-2")} />
+                    {!isMobile && <span>Relatório Completo</span>}
                   </DropdownMenuItem>
                 </DropdownMenuSubContent>
               </DropdownMenuPortal>
             </DropdownMenuSub>
             
             <DropdownMenuItem onClick={onPrint} className="cursor-pointer">
-              <Printer className="mr-2 h-4 w-4" />
-              <span>Imprimir (Navegador)</span>
+              <Printer className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Imprimir (Navegador)</span>}
             </DropdownMenuItem>
 
             <DropdownMenuSeparator />
 
             <DropdownMenuItem onClick={onExportJson} className="cursor-pointer">
-              <Download className="mr-2 h-4 w-4" />
-              <span>Exportar JSON</span>
+              <Download className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Exportar JSON</span>}
             </DropdownMenuItem>
 
             <DropdownMenuItem onClick={onTriggerImportJson} className="cursor-pointer">
-              <Upload className="mr-2 h-4 w-4" />
-              <span>Importar JSON</span>
+              <Upload className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Importar JSON</span>}
             </DropdownMenuItem>
             
             <DropdownMenuSeparator />
 
             <DropdownMenuItem onClick={onNewInspection} className="cursor-pointer text-destructive focus:text-destructive">
-              <PlusSquare className="mr-2 h-4 w-4" />
-              <span>Nova Vistoria (Limpar)</span>
+              <PlusSquare className={cn("h-4 w-4", !isMobile && "mr-2")} />
+              {!isMobile && <span>Nova Vistoria (Limpar)</span>}
             </DropdownMenuItem>
 
           </DropdownMenuContent>
