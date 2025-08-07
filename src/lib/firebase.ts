0b87f4637c18f49464b84517f9144ccbbdc08622
Gomes Contra Incêndio <comercialgomescontraincendio@gmail.com>
2025-08-07 16:16:46 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "em-andamento-ys2c7",
+  "appId": "1:704112085147:web:94ae1521b9d1c866ac9e3e",
+  "storageBucket": "em-andamento-ys2c7.firebasestorage.app",
+  "apiKey": "AIzaSyBjLgEJpSiC3ERVZqeMVdHXCpdVQWk1aN4",
+  "authDomain": "em-andamento-ys2c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "704112085147"
 };
+
 
 // Initialize Firebase
 let app: FirebaseApp;
